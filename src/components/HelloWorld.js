8678a598bdca9e781c4a3e4ae3455d1675836c47
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-02-06 05:16:33 +0100

    JSX Rules in React

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -9,9 +9,44 @@ const HelloWorld = () => {
     /* const myElement = <div><h1>Hello JSX !</h1></div>
     return myElement */
 
-    return React.createElement('div', null, 
+    /* return React.createElement('div', null, 
         React.createElement('h1', null, 'Hello React !')
+    ) */
+
+    /* To Return Multiple HTML Elements */
+    /* return (
+            <div>
+            <h1>Title</h1>
+            <h2>Sub Title</h2>
+            <p>Paragraph</p>
+            <img alt="" />
+        </div>
+    ) */
+
+    /* To Add a Event Handler on a Element */
+    function handleClick() {
+        alert("Button Cliked !")
+    }
+
+    /* To Use a JS Expression on JSX */
+    const name = "John Doe"
+    const addition = 1 + 5
+
+    return (
+        <>
+            {/* To Add a CSS Class on a Element */}
+            <h1 className="title">Title</h1>
+            <h2>Sub Title</h2>
+            <p>Paragraph</p>
+            {/* To Use a JS Expression on JSX */}
+            <p>Hello {name}</p>
+            <p>1 + 5 Equals : {addition}</p>
+            <img alt="" />
+            {/* To Add a Event Handler on a Element */}
+            <button onClick={handleClick}>Click</button>
+        </>
     )
+
 }
 
 export default HelloWorld
