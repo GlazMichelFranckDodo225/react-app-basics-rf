c639c212be0ce617c0bd6e01e464633076eeea69
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-02-22 07:03:11 +0100

    useState Hook - Functional Component V2

--- src/components/User.js
+++ src/components/User.js
@@ -1,22 +1,38 @@
 import React, { useState } from 'react'
 
 const User = () => {
-    const [firstName, setFirstName] = useState("Theodore");
+    /* const [firstName, setFirstName] = useState("Theodore");
     const [lastName, setLastName] = useState("Roosevelt");
-    const [email, setEmail] = useState("theodoreroosevelt@gmail.com");
+    const [email, setEmail] = useState("theodoreroosevelt@gmail.com"); */
+    const [user, setUser] = useState({
+        firstName: "Elon",
+        lastName: "Musk",
+        email: "elonmusk@gmail.com"
+    })
 
-    function updateUser() {
+    /* function updateUser() {
         setFirstName("Teddy");
         setLastName("Roose");
         setEmail("teddyroose@gmail.com")
+    } */
+    function updateUser() {
+        setUser({
+            firstName: "Elon - Updated",
+            lastName: "Musk - Updated",
+            email: "elonmuskupdated@gmail.com"
+        })
     }
 
     return (
         <div>
-            <h1>User Details</h1>
+            {/* <h1>User Details</h1>
             <p>First Name : {firstName}</p>
             <p>Last Name : {lastName}</p>
-            <p>Email : {email}</p>
+            <p>Email : {email}</p> */}
+            <h1>User Details</h1>
+            <p>First Name : {user.firstName}</p>
+            <p>Last Name : {user.lastName}</p>
+            <p>Email : {user.email}</p>
 
             <button type="submit" onClick={updateUser}>Update User</button>
         </div>
